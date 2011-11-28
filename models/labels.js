0e11b46b3db5cd9af49a2a1cbe0ffe4b19c8ef20
Brian <brian@trnsfr.com>
2011-11-28 09:30:12 -0800

    checked not highlighted

--- models/labels.js
+++ models/labels.js
@@ -17,8 +17,6 @@ _makeJSON = defn(function(keys, vals) {
 create = function(data, cb) {
 		var fs = split("\n", data);
 		var fs1 = map(split("\t"), fs);
-		log("fs1");
-		log(fs1);
 		var header = fs1.splice(0,1)[0];
 		var rest = filter('.length > 1', fs1);
 		var hd1 = map('.toLowerCase()', header);
--- public/js/controllers/pdf_controller.js
+++ public/js/controllers/pdf_controller.js
@@ -4,7 +4,7 @@ var getSelectedOptionIndex = compose('.val()', jQuerySelect('#labels'));
 
 var getLabelOption = compose(flip(index).p(Labels), getSelectedOptionIndex);
 
-var selectRecords = compose(map(Records.toRecord), map(ShowView.getAllFields), map('x.id'), jQuerySelect('.ui-state-highlight'));
+var selectRecords = compose(map(Records.toRecord), map(ShowView.getAllFields), map('x.id'), '.parents("tr")', jQuerySelect(".cbox:checked"));
 
 var makePdf = Combinators.f_g_h.p(Pdf.generate, getLabelOption, selectRecords);
 

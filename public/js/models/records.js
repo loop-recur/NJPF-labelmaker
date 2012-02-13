63de4f81447aeafd0b54d2593bebaad999cd7be4
Brian <brian@trnsfr.com>
2012-02-13 12:05:08 -0800

    suffex

--- public/js/models/records.js
+++ public/js/models/records.js
@@ -14,7 +14,19 @@ var _getRecord = compose(_transformToRecord, _getText);
 
 var _setCustomAddressee = ifelse(ShowView.useCustomField, set('owner', ShowView.customFieldText), id);
 
-var _ownerAddressSameAsLocation = Combinators.f_g_x_h_x(lambda("!=="), pluck('location'), pluck('owneraddr'));
+var _removeSuffix = function(str) {
+	var xs = split(" ", str);
+	xs.splice(-1,1);
+	return join(" ", xs);
+}
+
+var _everythingButSuffixEqual = function(str, str2) {
+	var s = _removeSuffix(str);
+	var s1 = _removeSuffix(str2)
+	return s != s1;
+}
+
+var _ownerAddressSameAsLocation = Combinators.f_g_x_h_x(_everythingButSuffixEqual, pluck('location'), pluck('owneraddr'));
 
 var _setCustomAddresseeIfNotOwner = ifelse(_ownerAddressSameAsLocation, _setCustomAddressee, id);
 
